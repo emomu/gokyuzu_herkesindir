af9ccc1465544bdd2eac13915f1f383ba640b1ee
Emirhan Soylu <121471344+emomu@users.noreply.github.com>
2024-12-14 12:00:56 +0300

    .2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:gokyuzu_herkesindir/config/routes/routes.dart';
-import 'package:gokyuzu_herkesindir/screens/splash/splash_screen.dart';
 import 'package:gokyuzu_herkesindir/utils/scale_config.dart';
 
 void main() {
